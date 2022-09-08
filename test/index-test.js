530899cbb7625372327136ece8fc54ac269773ed
Ramada Nguyen <ramadawin212@gmail.com>
2022-09-08 02:02:03 -0500

    commit tree a4c5c01f7431f05abd6f869a93959ce4f19c3750

--- test/index-test.js
+++ test/index-test.js
@@ -21,6 +21,6 @@ describe('introductionWithLanguageOptional(name, language)', function() {
 
 describe('introductionWithLanguageOptional(name, language)', function() {
   it('takes in two arguments, a name and a language, and the default value can be overridden with an argument', function() {
-    expect(introductionWithLanguageOptional("Gracie", "Python")).toEqual("Hi, my name is Gracie and I am learning to program in Python.");
+    expect(introductionWithLanguageOptional("Gracie" , "Python")).toEqual("Hi, my name is Gracie and I am learning to program in Python.");
   })
 })
